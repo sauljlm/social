a3f47abae5141587ae76da5757ac33d6c61f32a7
Saul Lopez Molina <saulsjlm2@gmail.com>
2020-01-10 14:30:50 -0600

    agrego paginas principales y la navegacion entre cada una

--- src/js/index.js
+++ src/js/index.js
@@ -10,6 +10,11 @@ import './registerServiceWorker';
 let singleton = new Singleton(); // eslint-disable-line
 let social = null;
 
+const btnPosts = document.querySelector('.nav__item--posts');
+const btnPeople = document.querySelector('.nav__item--people');
+const btnProfile = document.querySelector('.nav__item--profile');
+const contPages = document.querySelector('.slider');
+
 function appStar() {
   social = new Social();
   social.startRender();
@@ -19,5 +24,15 @@ async function init() {
   await singleton.loadData();
   appStar();
 }
+
+btnPosts.addEventListener('click', () => {
+  contPages.style.left = '0%';
+});
+btnPeople.addEventListener('click', () => {
+  contPages.style.left = '-100%';
+});
+btnProfile.addEventListener('click', () => {
+  contPages.style.left = '-200%';
+});
 
 document.addEventListener('DOMContentLoaded', init);
